3921957e4aaec81485fcd0168b2034390c71b888
SoJuSo <asdfg7123@gmail.com>
2023-12-25 22:48:56 +0900

    feat: 초기 splash화면 구성 현재는 그냥 간단하게 2초 뒤에 state를 바꾸고 본문 보여주는 형식으로 했는데, 추후 구현시에는 전역에서 API fetching이 완료되어 데이터가 보관된 것을 확인하면 바로 컴포넌트를 교체해주면 될 것 같아요!

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import { TEMP } from "@components/common";
 import { TestH1 } from "./styles/GlobalStyle";
 import { useUI } from "./components/common/uiContext";
 import useTheme from "./hooks/useTheme";
+import SplashPage from "@pages/SplashPage";
+import { useEffect, useState } from "react";
 
 const App = () => {
   // TEST 및 참고 //
   // UI 관련 로직을 uiCntext.tsx 에서 통합적으로 관리합니다 //
   const { modalView, displayModal } = useUI();
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsVisible(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
 
   // TEST 및 참고 //
   // 컴포넌트 내에서 theme 에 설정된 상수값을 사용하기 위해서는 //
@@ -15,7 +26,11 @@ const App = () => {
 
   console.log(modalView, displayModal, theme?.theme_mode);
 
-  return (
+  return isVisible ? (
+    <>
+      <SplashPage isVisible={isVisible} />
+    </>
+  ) : (
     <>
       <TestH1 className="border-2 border-black">Hello World!</TestH1>
       <h2>Hello</h2>
---
+++ src/pages/SplashPage/SplashPage.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SplashContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SplashText = styled.h2`
+  font-size: 60px;
+  color: #f48a4e;
+  font-weight: bold;
+`;
