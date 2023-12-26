86d3339b174ff68f76201dd00e238e9410a0bf03
Zoody <reopard506@gmail.com>
2023-12-26 22:38:19 +0900

    style: import 구문 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { GlobalStyle } from "./styles/GlobalStyle.tsx";
-import "./styles/tailwind.css";
 import { Provider } from "react-redux";
+
+import App from "./App.tsx";
+import "./styles/tailwind.css";
+import { GlobalStyle } from "./styles/GlobalStyle.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import store from "./store/index.ts";
 import { ManagedUIContext } from "./components/common/uiContext.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
