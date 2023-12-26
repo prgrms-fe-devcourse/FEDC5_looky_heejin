fe5ba01ab970ecb0c5fe6151ea0f7de285091e1e
SoJuSo <asdfg7123@gmail.com>
2023-12-26 09:19:32 +0900

    refactor: 폰트 단위를 컨벤션에 맞게 변경

--- src/pages/SplashPage/SplashPage.style.ts
+++ src/pages/SplashPage/SplashPage.style.ts
@@ -2,14 +2,12 @@ import styled from "styled-components";
 
 export const SplashContainer = styled.div`
   height: 100%;
-  display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
 export const SplashText = styled.h2`
-  font-size: 60px;
+  font-size: 3rem;
   color: #f48a4e;
   font-weight: bold;
 `;
