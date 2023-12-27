cb66998130d5f859bbe6d8763c529dea60d4d2c4
SoJuSo <asdfg7123@gmail.com>
2023-12-27 21:23:03 +0900

    feat: 로그인 로직 설정

--- src/pages/LoginPage/LoginPageView.tsx
+++ src/pages/LoginPage/LoginPageView.tsx
@@ -1,20 +1,42 @@
-import React, { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
+import Logo from "/logo.png";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { LogInPageContainer } from "./LoginPage.styles";
-
-interface IFormValue {
-  email: string;
+interface IFormData {
+  id: string;
   password: string;
 }
 
 const LoginPageView = () => {
+  const { register, watch, handleSubmit } = useForm<IFormData>();
+
+  const submitForm: SubmitHandler<IFormData> = data => {
+    console.log(data);
+  };
+  console.log(watch("id"));
+
   return (
     <LogInPageContainer>
-      <div>todo_logo</div>
-      <div>todo_이메일</div>
-      <div>todo_비밀번호</div>
-      <div>todo_로그인 버튼</div>
+      <img src={Logo} alt="루키 로고" loading="lazy" />
+      <form autoComplete="off" onSubmit={handleSubmit(submitForm)}>
+        <h3>로그인 폼_id</h3>
+        <input
+          type="text"
+          {...register("id", { required: true, maxLength: 20 })}
+        />
+      </form>
+      <form autoComplete="off" onSubmit={handleSubmit(submitForm)}>
+        <h3>로그인 폼_password</h3>
+        <input
+          type="text"
+          {...register("password", { required: true, maxLength: 20 })}
+        />
+      </form>
+      {/* <div>todo_이메일</div>
+      <div>todo_비밀번호</div> */}
+      <button onClick={handleSubmit(submitForm)}>
+        <div>todo_로그인 버튼</div>
+      </button>
       <Link to="/signin">회원가입 버튼</Link>
     </LogInPageContainer>
   );
