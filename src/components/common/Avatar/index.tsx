f6f1ed3fb123a1eca497f09ab8ef2e8b39c24124
Suyoung Shin <joyswim.dev@gmail.com>
2023-12-27 13:19:03 +0900

    refactor: 사이즈 지역 상수 선언

--- src/components/common/Avatar/index.tsx
+++ src/components/common/Avatar/index.tsx
@@ -4,20 +4,27 @@ import styled from "styled-components";
 type TShape = "circle" | "round" | "square";
 
 interface IAvatarProps {
-  size: number;
+  size: string;
   shape: TShape;
   src: string;
   alt: string;
 }
 
-const ShapeToStyle = {
+const ShapeToStyle: { [key: string]: string } = {
   circle: "50%",
   round: "5px",
   square: "0px",
 };
 
-const AvatarWrapper = styled.div<{ size: number; shape: TShape }>`
-  width: ${({ size }) => `${size}px`};
+const SizeToStyle: { [key: string]: string } = {
+  XS: "45px",
+  S: "60px",
+  M: "70px",
+  L: "80px",
+};
+
+const AvatarWrapper = styled.div<{ size: string; shape: TShape }>`
+  width: ${({ size }) => SizeToStyle[size]};
   position: relative;
   display: inline-block;
   border-style: none;
@@ -27,10 +34,10 @@ const AvatarWrapper = styled.div<{ size: number; shape: TShape }>`
 `;
 
 const Avatar: FC<IAvatarProps> = ({
-  size = 70,
+  size = "S",
   shape = "circle",
   src = "https://picsum.photos/100",
-  alt = "이미지",
+  alt = "프로필",
   ...props
 }) => {
   return (
