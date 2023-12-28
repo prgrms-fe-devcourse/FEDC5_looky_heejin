dee67db5e2166ba81cd850f3cc0135e0c887e3c0
Suyoung Shin <joyswim.dev@gmail.com>
2023-12-28 17:13:53 +0900

    feat: 최근 검색어 목록 보여주기

---
+++ src/pages/SearchPage/SearchPageRecent.tsx
@@ -0,0 +1,50 @@
+import { useEffect } from "react";
+import { styled } from "styled-components";
+
+interface SearchRecentProps {
+  searchHistory: string[];
+  onItemClick: (item: string) => void;
+}
+
+const LiContainer = styled.li`
+  color: ${props => props.theme.gray_500};
+
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
+`;
+
+const SearchPageRecent = ({
+  searchHistory,
+  onItemClick,
+  ...props
+}: SearchRecentProps) => {
+  const reversed = [...searchHistory].reverse();
+
+  useEffect(() => {
+    console.log(reversed);
+  }, []);
+
+  return (
+    <section>
+      <p style={{ margin: "2rem 0 1rem", fontWeight: "bold" }}>최근 검색어</p>
+      <ul>
+        {searchHistory.length > 0 ? (
+          reversed.map((item, index) => (
+            <LiContainer
+              key={index}
+              onClick={() => onItemClick(item)}
+              {...props}
+            >
+              {item}
+            </LiContainer>
+          ))
+        ) : (
+          <LiContainer {...props}>최근 검색어가 없습니다.</LiContainer>
+        )}
+      </ul>
+    </section>
+  );
+};
+
+export default SearchPageRecent;
