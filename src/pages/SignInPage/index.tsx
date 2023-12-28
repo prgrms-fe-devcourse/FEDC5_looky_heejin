1649499c42b0c080a97f809736dbee9f96b91243
SoJuSo <asdfg7123@gmail.com>
2023-12-28 17:01:26 +0900

    feat: 회원가입 타입 선언

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -1,4 +1,23 @@
+import { _SIGNIN } from "@/api/queries/signin";
+import { ISignIn } from "@/types";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface ISigninModify extends ISignIn {
+  passwordCheck: string;
+}
+
 const SignInPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ISigninModify>();
+
+  const fetchSignIn = async (signInData: ISignIn) => {
+    const res = await _SIGNIN(signInData);
+    console.log(res);
+  };
+
   return <div>SignInPage</div>;
 };
 
