30e887e84d8f3d0bb96a703e6e14a07b7811d2f8
suyoung <joyswim.dev@gmail.com>
2023-12-28 23:51:32 +0900

    style: padding, margin 수정

--- src/pages/SearchPage/SearchPage.styles.ts
+++ src/pages/SearchPage/SearchPage.styles.ts
@@ -1,37 +1,20 @@
 import styled from "styled-components";
 import { Col, Row } from "@/styles/GlobalStyle";
-import { NAV_HEIGHT } from "@/constants/uiConstants";
 
 export const SearchWrap = styled(Col)`
   height: 100vh;
   border: 1px solid black;
   justify-content: space-between;
-  padding-left: 1rem;
-  padding-right: 1rem;
 `;
 
 export const SearchViewWrap = styled.div`
   flex-grow: 1;
-`;
-
-export const SearchTabWrap = styled(Row)`
-  margin-top: 1rem;
-  height: ${`${NAV_HEIGHT - 1}rem`};
-`;
-
-export const SearchTab = styled.div`
-  background-color: ${({ theme }) => theme.gray_200};
-  flex-grow: 1;
-  text-align: center;
-  line-height: ${`${NAV_HEIGHT - 1}rem`};
-  cursor: pointer;
-  &:first-of-type {
-    border-right: 2px solid ${({ theme }) => theme.white_primary};
-  }
+  padding-left: 1rem;
+  padding-right: 1rem;
 `;
 
 export const InputWrap = styled(Row)`
-  margin-top: 1rem;
+  margin: 1rem;
   height: ${`{NAV_HEIGHT}rem`};
   align-items: center;
   flex-grow: 0;
