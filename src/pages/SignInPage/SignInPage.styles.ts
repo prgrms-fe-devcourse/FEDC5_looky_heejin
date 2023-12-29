d44fe2c2fdbb9ec87e304740c600d20d12021bfd
SoJuSo <asdfg7123@gmail.com>
2023-12-29 12:11:09 +0900

    feat: 파일 구조 변경 login에서 재사용 할 수 있는 로직 가져왔는데, 추후 common 파일로 분리 필요

---
+++ src/pages/SignInPage/SignInPage.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Col } from "@/styles/GlobalStyle";
+
+export const SignInPageContainer = styled(Col)`
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
