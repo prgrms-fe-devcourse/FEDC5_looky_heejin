2a1007f5d8a05608be6a5ae589cd7d30928daa90
SoJuSo <asdfg7123@gmail.com>
2023-12-29 14:21:22 +0900

    feat: 각 입력창 마다 에러 조건,메세지 추가

--- src/pages/SignInPage/SignInPageView.tsx
+++ src/pages/SignInPage/SignInPageView.tsx
@@ -12,6 +12,7 @@ const SignInPage = () => {
   const {
     register,
     handleSubmit,
+    getValues,
     formState: { errors },
   } = useForm<ISigninModify>({ mode: "onChange" });
 
@@ -40,7 +41,86 @@ const SignInPage = () => {
 
   return (
     <SignInPageContainer>
-      <div>SignInPage</div>
+      <form autoComplete="off" onSubmit={handleSubmit(onValid, onInValid)}>
+        <input
+          type="text"
+          placeholder="이메일"
+          {...register("email", {
+            required: {
+              value: true,
+              message: "이메일을 입력해주세요",
+            },
+            minLength: {
+              value: 8,
+              message: "너무 짧아요!",
+            },
+            pattern: {
+              value: emailRegex,
+              message: "이메일 양식에 맞게 입력해주세요",
+            },
+            maxLength: {
+              value: 50,
+              message: "너무 길어요!",
+            },
+          })}
+        />
+        <input
+          type="text"
+          placeholder="닉네임"
+          {...register("fullName", {
+            required: {
+              value: true,
+              message: "닉네임을 입력해주세요",
+            },
+            minLength: {
+              value: 2,
+              message: "너무 짧아요!",
+            },
+            maxLength: {
+              value: 20,
+              message: "너무 길어요!",
+            },
+          })}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          {...register("password", {
+            required: {
+              value: true,
+              message: "비밀번호를 입력해주세요",
+            },
+            minLength: {
+              value: 8,
+              message: "비밀번호는 8자 이상이여야 해요!",
+            },
+            pattern: {
+              value: passwordRegex,
+              message:
+                "소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.",
+            },
+            maxLength: {
+              value: 30,
+              message: "비밀번호가 너무 길어요!",
+            },
+          })}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 확인"
+          {...register("passwordCheck", {
+            required: true,
+            minLength: 8,
+            pattern: passwordRegex,
+            maxLength: 30,
+            validate: value => {
+              const { password } = getValues();
+              return password === value || "비밀번호가 일치하지 않습니다!";
+            },
+          })}
+        />
+        <button onSubmit={handleSubmit(onValid, onInValid)}>회원가입</button>
+      </form>
     </SignInPageContainer>
   );
 };
