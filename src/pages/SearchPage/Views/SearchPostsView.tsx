20341f7c32678a92a1e063250d8d43813c3c7b59
Suyoung Shin <joyswim.dev@gmail.com>
2023-12-29 11:42:32 +0900

    feat: 결과 페이지 사용자, 게시글 뷰 추가

---
+++ src/pages/SearchPage/Views/SearchPostsView.tsx
@@ -0,0 +1,12 @@
+import { SearchTab } from "@/components/SearchPage";
+
+const SearchPostsView = () => {
+  return (
+    <>
+      <SearchTab option="post" />
+      <section>게시글을 보여주세요</section>
+    </>
+  );
+};
+
+export default SearchPostsView;
---
+++ src/pages/SearchPage/Views/SearchUsersView.tsx
@@ -0,0 +1,12 @@
+import { SearchTab } from "@/components/SearchPage";
+
+const SearchUsersView = () => {
+  return (
+    <>
+      <SearchTab option="user" />
+      <section>사용자를 보여주세요</section>
+    </>
+  );
+};
+
+export default SearchUsersView;
