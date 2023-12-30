5361d7b172a0be9e24472ccf9377388652746c84
SoJuSo <asdfg7123@gmail.com>
2023-12-30 21:27:28 +0900

    feat: 비밀번호 확인 조건 상수화

--- src/pages/SignInPage/SignInPage.const.ts
+++ src/pages/SignInPage/SignInPage.const.ts
@@ -56,8 +56,28 @@ const PASSWORD_VALIDATION_OPTION = {
   },
 };
 
+const PASSWORD_CHECK_VALIDATION_OPTION = {
+  required: {
+    value: true,
+    message: "비밀번호를 입력해주세요",
+  },
+  minLength: {
+    value: 8,
+    message: "비밀번호는 8자 이상이여야 해요!",
+  },
+  pattern: {
+    value: passwordRegex,
+    message: "소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.",
+  },
+  maxLength: {
+    value: 30,
+    message: "비밀번호가 너무 길어요!",
+  },
+};
+
 export default {
   EMAIL_VALIDATION_OPTION,
   FULLNAME_VALIDATION_OPTION,
   PASSWORD_VALIDATION_OPTION,
+  PASSWORD_CHECK_VALIDATION_OPTION,
 };
