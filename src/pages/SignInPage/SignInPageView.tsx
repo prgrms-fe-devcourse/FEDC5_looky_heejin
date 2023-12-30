e4fa0023a97fda830b97d355072111232104c4c6
SoJuSo <asdfg7123@gmail.com>
2023-12-30 17:04:38 +0900

    feat: 로그인 폼 태그 내부 상수화 리팩토링, watch옵션 사용한 비밀번호 확인 코드 구성

--- src/pages/SignInPage/SignInPageView.tsx
+++ src/pages/SignInPage/SignInPageView.tsx
@@ -2,7 +2,9 @@ import { _SIGNIN } from "@/api/queries/signin";
 import { ISignIn } from "@/types";
 import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
 import { useMutation } from "@tanstack/react-query";
-import { SignInPageContainer } from "./SignInPage.styles";
+import { FormContainer, SignInPageContainer } from "./SignInPage.styles";
+import SignInPageConstant from "./SignInPage.const";
+import { useEffect } from "react";
 
 interface ISigninModify extends ISignIn {
   passwordCheck: string;
@@ -10,11 +12,22 @@ interface ISigninModify extends ISignIn {
 
 const SignInPage = () => {
   const {
+    watch,
     register,
     handleSubmit,
     getValues,
+    setError,
+    clearErrors,
     formState: { errors },
-  } = useForm<ISigninModify>({ mode: "onChange" });
+  } = useForm<ISigninModify>({
+    mode: "onChange",
+    defaultValues: {
+      email: "",
+      fullName: "",
+      password: "",
+      passwordCheck: "",
+    },
+  });
 
   const mutation = useMutation({
     mutationFn: async (formData: ISignIn) => await _SIGNIN(formData),
@@ -34,93 +47,73 @@ const SignInPage = () => {
     console.log("양식이 안맞으므로 호출 X:", error);
   };
 
-  // 유효성 검사 정규식
-  const emailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[!@#])[\da-zA-Z!@#]{8,}$/;
+  useEffect(() => {
+    if (
+      watch("password") !== watch("passwordCheck") &&
+      watch("passwordCheck")
+    ) {
+      setError("passwordCheck", {
+        type: "password-mismatch",
+        message: "비밀번호가 일치하지 않습니다!",
+      });
+    } else {
+      // 비밀번호 일치시 오류 제거
+      clearErrors("passwordCheck");
+    }
+  }, [watch("password"), watch("passwordCheck")]);
 
   return (
     <SignInPageContainer>
-      <form autoComplete="off" onSubmit={handleSubmit(onValid, onInValid)}>
+      <FormContainer
+        autoComplete="off"
+        onSubmit={handleSubmit(onValid, onInValid)}
+      >
         <input
           type="text"
           placeholder="이메일"
-          {...register("email", {
-            required: {
-              value: true,
-              message: "이메일을 입력해주세요",
-            },
-            minLength: {
-              value: 8,
-              message: "너무 짧아요!",
-            },
-            pattern: {
-              value: emailRegex,
-              message: "이메일 양식에 맞게 입력해주세요",
-            },
-            maxLength: {
-              value: 50,
-              message: "너무 길어요!",
-            },
-          })}
+          {...register("email", SignInPageConstant.EMAIL_VALIDATION_OPTION)}
         />
+        {errors.email?.message ? <span>{errors.email?.message}</span> : null}
         <input
           type="text"
           placeholder="닉네임"
-          {...register("fullName", {
-            required: {
-              value: true,
-              message: "닉네임을 입력해주세요",
-            },
-            minLength: {
-              value: 2,
-              message: "너무 짧아요!",
-            },
-            maxLength: {
-              value: 20,
-              message: "너무 길어요!",
-            },
-          })}
+          {...register(
+            "fullName",
+            SignInPageConstant.FULLNAME_VALIDATION_OPTION
+          )}
         />
+        {errors.fullName?.message ? (
+          <span>{errors.fullName?.message}</span>
+        ) : null}
         <input
           type="password"
           placeholder="비밀번호"
-          {...register("password", {
-            required: {
-              value: true,
-              message: "비밀번호를 입력해주세요",
-            },
-            minLength: {
-              value: 8,
-              message: "비밀번호는 8자 이상이여야 해요!",
-            },
-            pattern: {
-              value: passwordRegex,
-              message:
-                "소문자, 숫자, 특수문자를 각 하나 포함한 8자리 이상이여야 합니다.",
-            },
-            maxLength: {
-              value: 30,
-              message: "비밀번호가 너무 길어요!",
-            },
-          })}
+          {...register(
+            "password",
+            SignInPageConstant.PASSWORD_VALIDATION_OPTION
+          )}
         />
+        {errors.password?.message ? (
+          <span>{errors.password?.message}</span>
+        ) : null}
         <input
           type="password"
           placeholder="비밀번호 확인"
           {...register("passwordCheck", {
             required: true,
-            minLength: 8,
-            pattern: passwordRegex,
-            maxLength: 30,
-            validate: value => {
-              const { password } = getValues();
-              return password === value || "비밀번호가 일치하지 않습니다!";
+            validate: {
+              matchPassword: value => {
+                const { password } = getValues();
+                return password === value || "비밀번호가 일치하지 않습니다!";
+              },
             },
           })}
         />
+        {errors.passwordCheck ? (
+          <span>{errors.passwordCheck.message}</span>
+        ) : null}
         <button onSubmit={handleSubmit(onValid, onInValid)}>회원가입</button>
-      </form>
+      </FormContainer>
     </SignInPageContainer>
   );
 };
