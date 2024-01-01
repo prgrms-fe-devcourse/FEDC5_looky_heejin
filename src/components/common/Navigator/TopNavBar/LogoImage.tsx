d80b017f9314463ddd517dd49697eb831dcdbda9
hellosonic-r <hellommmh@gmail.com>
2024-01-01 20:01:29 +0900

    design : 로고 사이즈 수정

--- src/components/common/Navigator/TopNavBar/LogoImage.tsx
+++ src/components/common/Navigator/TopNavBar/LogoImage.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "/logo.png";
 
 interface ILogoImageProps {
   onClick: React.MouseEventHandler<HTMLImageElement>;
 }
 
 const StyledImg = styled.img`
-  width: 60%;
+  width: 45%;
   margin: auto auto;
   cursor: pointer;
 `;
 
 const LogoImage = ({ onClick }: ILogoImageProps) => {
-  return <StyledImg src="logo.png" alt="logo" onClick={onClick} />;
+  return <StyledImg src={Logo} alt="루키 로고" onClick={onClick} />;
 };
 
 export default LogoImage;
