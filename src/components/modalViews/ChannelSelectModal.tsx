75a872ee93f5fd86e1cab5afaf7697c066f02503
Zoody <reopard506@gmail.com>
2024-01-01 13:04:49 +0900

    refactor: 실제 채널 데이터 연동

--- src/components/modalViews/ChannelSelectModal.tsx
+++ src/components/modalViews/ChannelSelectModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { ModalLayout } from "../common/Modal";
 import { Button } from "../common";
@@ -6,63 +6,39 @@ import { useUI } from "../common/uiContext";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { useNewPost } from "@/hooks/useNewPost";
-
-interface IChannelSelectFormProps {
-  channelId: string;
-  channelName: string;
-}
-
-const MOCK_DATA = [
-  {
-    id: "1",
-    name: "아웃도어",
-  },
-  {
-    id: "2",
-    name: "아메카지",
-  },
-  {
-    id: "3",
-    name: "캐주얼",
-  },
-  {
-    id: "4",
-    name: "스트릿",
-  },
-  {
-    id: "5",
-    name: "고프코어",
-  },
-  {
-    id: "6",
-    name: "고프코어",
-  },
-  {
-    id: "7",
-    name: "빈티지",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { _GET } from "@/api";
+import { IChannel } from "@/types/channel";
 
 const ChannelSelectModal = () => {
   const { closeModal } = useUI();
   const { setChannel } = useNewPost();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["channels"],
+    queryFn: async () => await _GET("/channels"),
+  });
 
   const handleChannelSelectClick = (channelId: string, channelName: string) => {
     setChannel(channelId, channelName);
     closeModal();
   };
 
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <ModalLayout modalTitle="채널 선택">
       <section className="relative w-[300px] h-[300px] pt-8">
         <ul className="flex flex-row flex-wrap gap-2">
-          {MOCK_DATA.map(({ id, name }) => (
-            <ChannelItem key={id}>
+          {data?.data.map(({ _id, name }: IChannel) => (
+            <ChannelItem key={_id}>
               <Button
                 variant="flat"
                 style={{ padding: "0.3rem 1rem" }}
                 className="bg-[#B3B3B390]"
-                onClick={() => handleChannelSelectClick(id, name)}
+                onClick={() => handleChannelSelectClick(_id, name)}
               >
                 <span>{name}</span>
               </Button>
