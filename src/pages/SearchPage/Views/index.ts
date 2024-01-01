4e4e1c02e6532345a577c6fa8e71a18e88b8d202
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-01 13:26:53 +0900

    fix: export 경로 수정

---
+++ src/pages/SearchPage/Views/index.ts
@@ -0,0 +1,4 @@
+export { default as SearchResultsView } from "./SearchResultsView";
+export { default as SearchRecentView } from "./SearchRecentView";
+export { default as SearchUsersView } from "./SearchUsersView";
+export { default as SearchPostsView } from "./SearchPostsView";
