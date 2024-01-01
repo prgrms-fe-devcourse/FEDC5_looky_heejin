6e063de7ddcce4fded55de6014195125711a7bb1
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-01 12:20:27 +0900

    feat: 사용자, 게시글 탭 뷰 추가

--- src/pages/SearchPage/Views/SearchPostsView.tsx
+++ src/pages/SearchPage/Views/SearchPostsView.tsx
@@ -1,9 +1,6 @@
-import { SearchTab } from "@/components/SearchPage";
-
 const SearchPostsView = () => {
   return (
     <>
-      <SearchTab option="post" />
       <section>게시글을 보여주세요</section>
     </>
   );
