739fecb63e9acc62ae383867fb8dcf623256cf05
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-01 12:20:27 +0900

    feat: 사용자, 게시글 탭 뷰 추가

--- src/pages/SearchPage/Views/SearchUsersView.tsx
+++ src/pages/SearchPage/Views/SearchUsersView.tsx
@@ -1,9 +1,6 @@
-import { SearchTab } from "@/components/SearchPage";
-
 const SearchUsersView = () => {
   return (
     <>
-      <SearchTab option="user" />
       <section>사용자를 보여주세요</section>
     </>
   );
