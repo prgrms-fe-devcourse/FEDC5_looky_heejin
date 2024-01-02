47a7dfafcbbb6ae392e0cb776ea233d7e4e9ec15
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-02 12:59:15 +0900

    feat: 검색 User 타입 선언

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,24 @@ export interface ISignIn {
   fullName: string;
   password: string;
 }
+export interface IUser {
+  coverImage?: string;
+  image?: string;
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: [];
+  likes: [];
+  comments: string[];
+  followers: [];
+  following: [];
+  notifications: [];
+  messages: [];
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  __v?: number;
+}
