2c1cc279a7fc5c0d0da7640246a5ad581a5d2d10
SoJuSo <asdfg7123@gmail.com>
2024-01-02 20:26:47 +0900

    style: 카드 기능 구현, 구조 생성

---
+++ src/components/common/PostSimpleCard/PostSimpleCard.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  flex-basis: calc(50% - 11px);
+  margin: 0px 5.5px 12px;
+  width: calc(50% - 11px);
+  /* flex-basis: calc(50% - 12px);
+  width: calc(50% - 12px);
+  max-height: 30vh;
+  max-width: 280px;
+  margin: 0px 6px 8px; */
+  border: 1px solid red;
+`;
