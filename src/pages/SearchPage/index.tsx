1064022a53655e8273f7ddffbf5c3023f32c8a45
hellosonic-r <hellommmh@gmail.com>
2024-01-02 18:53:16 +0900

    feat : SearchPage에서 리덕스 스토어에 저장된 값을 가져올 수 있습니다. 예시로 남깁니다!

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,5 +1,8 @@
+import { useSearchValue } from "@/hooks/useSearchValue";
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { keyword } = useSearchValue();
+  return <div>navbar의 검색창에 입력 후 submit된 값 : {keyword}</div>;
 };
 
 export default SearchPage;
