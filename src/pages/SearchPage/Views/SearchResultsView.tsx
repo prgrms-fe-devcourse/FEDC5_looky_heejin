6a6dda34bfbf402dc3284dd9f6724e8e0479d198
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-02 18:12:57 +0900

    feat: 사용자, 게시글 데이터 분리

--- src/pages/SearchPage/Views/SearchResultsView.tsx
+++ src/pages/SearchPage/Views/SearchResultsView.tsx
@@ -3,40 +3,129 @@ import { useEffect, useState } from "react";
 import { SearchPostsView, SearchUsersView } from ".";
 import { useQuery } from "@tanstack/react-query";
 import { _SEARCH, _SEARCH_USERS } from "@/api/queries/search";
-import { useSearchParams } from "react-router-dom";
-import { IUser } from "@/types";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { IPost, IUser } from "@/types";
+
+const DUMMY_DATA = [
+  {
+    likes: [],
+    comments: [],
+    _id: "65939d992ed4d31ff83cb873",
+    title: "캐주얼_테스트",
+    image:
+      "https://res.cloudinary.com/learnprogrammers/image/upload/v1704172952/post/f05d1be2-9cf9-415d-bc72-354fcc23a84a.jpg",
+    imagePublicId: "post/f05d1be2-9cf9-415d-bc72-354fcc23a84a",
+    channel: "659236a82ed4d31ff83cb06a",
+    author: "64edba4f8f63f012a6741681",
+    createdAt: "2024-01-02T05:22:33.200Z",
+    updatedAt: "2024-01-02T05:22:33.200Z",
+    __v: 0,
+  },
+  {
+    likes: [],
+    comments: [],
+    _id: "65939e1f2ed4d31ff83cb883",
+    title: "캐주얼_테스트_2",
+    image:
+      "https://res.cloudinary.com/learnprogrammers/image/upload/v1704173086/post/4084fd38-0cc6-4a25-9e74-47dd9d244fd0.jpg",
+    imagePublicId: "post/4084fd38-0cc6-4a25-9e74-47dd9d244fd0",
+    channel: "659236a82ed4d31ff83cb06a",
+    author: "64edba4f8f63f012a6741681",
+    createdAt: "2024-01-02T05:24:47.022Z",
+    updatedAt: "2024-01-02T05:24:47.022Z",
+    __v: 0,
+  },
+  {
+    role: "Regular",
+    emailVerified: false,
+    banned: false,
+    isOnline: false,
+    posts: [],
+    likes: [],
+    comments: [],
+    followers: [],
+    following: [],
+    notifications: [],
+    messages: [],
+    _id: "6593c7492ed4d31ff83cb929",
+    fullName: "캐주얼시민",
+    email: "qwer@qweqwe.com",
+    createdAt: "2024-01-02T08:20:25.581Z",
+    updatedAt: "2024-01-02T08:20:25.581Z",
+    __v: 0,
+  },
+];
 
 const SearchResultsView = () => {
-  const [searchData, setSearchData] = useState<IUser[]>([]);
+  const [usersData, setUsersData] = useState<IUser[]>([]);
+  const [postsData, setPostsData] = useState<IPost[]>([]);
   const [showUsers, setShowUsers] = useState(true);
   const [searchParams, _] = useSearchParams();
-  const query = searchParams.get("keyword");
-  const { data, isLoading, error, isSuccess } = useQuery({
-    queryKey: ["search/all", query ?? ""],
-    queryFn: ({ queryKey }) => _SEARCH(queryKey[1]),
-  });
+  // const query = searchParams.get("keyword");
+  // const { data, isLoading, error, isSuccess } = useQuery({
+  //   queryKey: ["search/all", query ?? ""],
+  //   queryFn: ({ queryKey }) => _SEARCH(queryKey[1]),
+  // });
 
+  const navigate = useNavigate();
+
+  interface FilteredData {
+    users: any[];
+    posts: any[];
+  }
+
+  // 사용자, 게시글 데이터를 분리
   useEffect(() => {
-    if (isSuccess) {
-      const copy = [...data];
-      if (copy.length < 0) return;
-      const addImageUsers = copy.map(user => ({
-        ...user,
-        coverImage: "https://picsum.photos/100",
-        image: "https://picsum.photos/100",
-      }));
+    const copy = [...DUMMY_DATA];
 
-      setSearchData(addImageUsers);
-    }
-  }, [isSuccess]);
+    const filteredData: FilteredData = copy.reduce<FilteredData>(
+      (results, item) => {
+        if (item.title) {
+          results.posts = results.posts.concat(item);
+        } else {
+          results.users = results.users.concat(item);
+        }
+        return results;
+      },
+      { users: [], posts: [] }
+    );
 
-  if (isLoading) {
-    return <div>Loading</div>;
-  }
+    const filteredUsers = filteredData.users;
+    const filteredPosts = filteredData.posts;
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    setUsersData(filteredUsers);
+    setPostsData(filteredPosts);
+  }, [DUMMY_DATA]);
+
+  // useEffect(() => {
+  //   if (isSuccess) {
+  //     const copy = [...data];
+  //     if (copy.length < 0) return;
+
+  //     // const addImageUsers = copy.map(user => ({
+  //     //   ...user,
+  //     //   coverImage: "https://picsum.photos/100",
+  //     //   image: "https://picsum.photos/100",
+  //     // }));
+
+  //     const filteredUsers = copy.filter(item => item.title);
+  //     const filteredPosts = copy.filter(item => !item.title);
+
+  //     console.log(filteredUsers);
+  //     console.log(filteredPosts);
+
+  //     setUsersData(filteredUsers);
+  //     setPostsData(filteredPosts);
+  //   }
+  // }, [isSuccess]);
+
+  // if (isLoading) {
+  //   return <div>Loading</div>;
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>;
+  // }
 
   const handleTabClick = () => {
     setShowUsers(!showUsers);
@@ -44,7 +133,7 @@ const SearchResultsView = () => {
 
   const handleUserClick = (userId: string) => {
     console.log("UserClicked!" + userId);
-    // Todo: Profile 페이지 이동
+    navigate(`/profile/${userId}`);
   };
 
   return (
@@ -55,9 +144,9 @@ const SearchResultsView = () => {
         <SearchTab option="post" onClick={handleTabClick}></SearchTab>
       )}
       {showUsers ? (
-        <SearchUsersView searchData={searchData} onClick={handleUserClick} />
+        <SearchUsersView usersData={usersData} onClick={handleUserClick} />
       ) : (
-        <SearchPostsView searchData={searchData} />
+        <SearchPostsView postsData={postsData} />
       )}
     </div>
   );
