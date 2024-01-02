9f52268d6c00192c2b2ff64bc75218a1509c256b
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-02 13:08:59 +0900

    feat: 검색페이지 게시글 뷰 데이터 전달

--- src/pages/SearchPage/Views/SearchPostsView.tsx
+++ src/pages/SearchPage/Views/SearchPostsView.tsx
@@ -1,7 +1,20 @@
-const SearchPostsView = () => {
+import { IUser } from "@/types";
+import { useEffect } from "react";
+
+interface IPostsProps {
+  searchData: IUser[];
+}
+
+const SearchPostsView = ({ searchData }: IPostsProps) => {
+  useEffect(() => {
+    console.log("Posts:  ", searchData);
+  }, [searchData]);
+
   return (
     <>
-      <section>게시글을 보여주세요</section>
+      <section>
+        <div>게시글을 보여주세요</div>
+      </section>
     </>
   );
 };
