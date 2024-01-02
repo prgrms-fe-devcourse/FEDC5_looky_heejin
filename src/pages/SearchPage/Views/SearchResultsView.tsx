d4a6e1e75079205639ac000d15e204f7f822754c
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-02 13:02:52 +0900

    feat: 검색 데이터 api 호출

--- src/pages/SearchPage/Views/SearchResultsView.tsx
+++ src/pages/SearchPage/Views/SearchResultsView.tsx
@@ -1,9 +1,42 @@
 import { SearchTab } from "@/components/SearchPage";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SearchPostsView, SearchUsersView } from ".";
+import { useQuery } from "@tanstack/react-query";
+import { _SEARCH, _SEARCH_USERS } from "@/api/queries/search";
+import { useSearchParams } from "react-router-dom";
+import { IUser } from "@/types";
 
 const SearchResultsView = () => {
+  const [searchData, setSearchData] = useState<IUser[]>([]);
   const [showUsers, setShowUsers] = useState(true);
+  const [searchParams, _] = useSearchParams();
+  const query = searchParams.get("keyword");
+  const { data, isLoading, error, isSuccess } = useQuery({
+    queryKey: ["search/all", query ?? ""],
+    queryFn: ({ queryKey }) => _SEARCH(queryKey[1]),
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      const copy = [...data];
+      if (copy.length < 0) return;
+      const addImageUsers = copy.map(user => ({
+        ...user,
+        coverImage: "https://picsum.photos/100",
+        image: "https://picsum.photos/100",
+      }));
+
+      setSearchData(addImageUsers);
+    }
+  }, [isSuccess]);
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   const handleTabClick = () => {
     setShowUsers(!showUsers);
@@ -16,7 +49,11 @@ const SearchResultsView = () => {
       ) : (
         <SearchTab option="post" onClick={handleTabClick}></SearchTab>
       )}
-      {showUsers ? <SearchUsersView /> : <SearchPostsView />}
+      {showUsers ? (
+        <SearchUsersView searchData={searchData} />
+      ) : (
+        <SearchPostsView searchData={searchData} />
+      )}
     </div>
   );
 };
