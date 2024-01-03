48263b120fbfcf3ee9299f5950c0b7995aea68aa
SoJuSo <asdfg7123@gmail.com>
2024-01-04 00:50:17 +0900

    feat: 해당 포스팅의 정보를 받은 뒤 정보를 바탕으로 필요한 API를 호출하는 코드 추가 design: 구글 아이콘 추가

--- src/components/common/PostSimpleCard/PostSimpleCard.tsx
+++ src/components/common/PostSimpleCard/PostSimpleCard.tsx
@@ -1,20 +1,110 @@
-import React, { useState } from "react";
-import { CardContainer } from "./PostSimpleCard.styles";
+import { CardContainer, CardImageContainer } from "./PostSimpleCard.styles";
 import PostSimpleCardConst from "./PostSimpleCard.const";
-import { Image } from "..";
+import { _USERDATA } from "@/api/queries/userData";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { Avatar } from "..";
 
 const PostSimpleCard = () => {
-  return (
-    <>
-      <CardContainer>
-        <Image
-          src={PostSimpleCardConst.data_example.image}
-          width={200}
-          height={300}
-        />
-      </CardContainer>
-    </>
-  );
+  const [imageUrl, setimageUrl] = useState<string>();
+  const [userName, setUserName] = useState<string>();
+  const mutation = useMutation({
+    mutationFn: async (params: string) => await _USERDATA(params),
+    onSuccess(data) {
+      console.log("API 성공: ", data);
+      setimageUrl(data.image);
+      setUserName(data.fullName);
+      // console.log(imageUrl);
+    },
+    onError(error) {
+      console.error("API 에러: ", error);
+    },
+  });
+
+  useEffect(() => {
+    mutation.mutate(PostSimpleCardConst.data_example.author);
+  }, []);
+
+  if (mutation.isError) {
+    return <div>실패요 수고</div>;
+  }
+
+  if (mutation.isPending) {
+    return <div>기다리셈</div>;
+  }
+
+  if (mutation.isSuccess) {
+    console.log(imageUrl);
+    return (
+      <>
+        <CardContainer $basis="half">
+          <div>
+            <img
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              src={PostSimpleCardConst.data_example.image}
+            />
+          </div>
+          <CardImageContainer>
+            <Avatar
+              size={"XS"}
+              shape={"round"}
+              src={imageUrl ? imageUrl : ""}
+              alt={"사용자 아이콘"}
+            ></Avatar>
+            <span
+              style={{
+                boxSizing: "border-box",
+                rotate: "-45deg",
+                position: "absolute",
+                top: "5%",
+                // bottom: "0",
+                right: "1%",
+              }}
+              className="material-symbols-rounded"
+            >
+              send
+            </span>
+            <span
+              style={{
+                boxSizing: "border-box",
+                position: "absolute",
+                top: "7%",
+                // bottom: "0",
+                right: "14%",
+              }}
+              className="material-symbols-rounded"
+            >
+              comment
+            </span>
+            <span
+              style={{
+                boxSizing: "border-box",
+                position: "absolute",
+                top: "-40%",
+                right: "2%",
+              }}
+              className="material-symbols-rounded"
+            >
+              sell
+            </span>
+            <span
+              style={{
+                boxSizing: "border-box",
+                position: "absolute",
+                top: "-80%",
+                right: "2%",
+              }}
+              className="material-symbols-rounded"
+            >
+              favorite
+            </span>
+            <span>{userName}</span>
+            <span>{PostSimpleCardConst.data_example.title}</span>
+          </CardImageContainer>
+        </CardContainer>
+      </>
+    );
+  }
 };
 
 export default PostSimpleCard;
