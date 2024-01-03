0614dbfd3e7864c25a8fac9f694217e3915ee61a
SoJuSo <asdfg7123@gmail.com>
2024-01-04 00:50:50 +0900

    design: 전체 컨테이너에 조건 추가, 반응형 요소 추가

--- src/components/common/PostSimpleCard/PostSimpleCard.styles.ts
+++ src/components/common/PostSimpleCard/PostSimpleCard.styles.ts
@@ -1,13 +1,31 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const CardContainer = styled.div`
-  flex-basis: calc(50% - 11px);
+export const CardContainer = styled.div<{ $basis: "half" | "full" }>`
+  flex-direction: column;
+  ${({ $basis }) => {
+    if ($basis === "half") {
+      return css`
+        max-height: 70%;
+        flex-basis: calc(50% - 11px);
+      `;
+    } else {
+      return css`
+        max-height: 70%;
+        flex-basis: calc(100% - 11px);
+      `;
+    }
+  }};
+
+  @media screen and (max-width: 500px) {
+    max-height: 70%;
+    flex-basis: calc(100% - 11px);
+  }
+  box-sizing: border-box;
   margin: 0px 5.5px 12px;
-  width: calc(50% - 11px);
-  /* flex-basis: calc(50% - 12px);
-  width: calc(50% - 12px);
-  max-height: 30vh;
-  max-width: 280px;
-  margin: 0px 6px 8px; */
-  border: 1px solid red;
+  border: 1px solid royalblue;
+`;
+
+export const CardImageContainer = styled.div`
+  position: relative;
+  min-height: auto;
 `;
