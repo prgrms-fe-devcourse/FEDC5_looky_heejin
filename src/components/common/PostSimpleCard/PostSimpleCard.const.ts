9f044a75d18c27d638e723c8525d15e565e18a2a
SoJuSo <asdfg7123@gmail.com>
2024-01-04 12:23:38 +0900

    chore: 예제 코드 변경

--- src/components/common/PostSimpleCard/PostSimpleCard.const.ts
+++ src/components/common/PostSimpleCard/PostSimpleCard.const.ts
@@ -1,15 +1,15 @@
 const data_example = {
   likes: [],
   comments: [],
-  _id: "65939e1f2ed4d31ff83cb883",
-  title: "캐주얼_테스트_2",
+  _id: "6595ffb22ed4d31ff83d1a1c",
+  title: "캐주얼_테스트_3",
   image:
-    "https://res.cloudinary.com/learnprogrammers/image/upload/v1704173086/post/4084fd38-0cc6-4a25-9e74-47dd9d244fd0.jpg",
-  imagePublicId: "post/4084fd38-0cc6-4a25-9e74-47dd9d244fd0",
+    "https://res.cloudinary.com/learnprogrammers/image/upload/v1704329137/post/75639fd8-3462-40e8-b69a-428b3525ce85.jpg",
+  imagePublicId: "post/75639fd8-3462-40e8-b69a-428b3525ce85",
   channel: "659236a82ed4d31ff83cb06a",
   author: "64edba4f8f63f012a6741681",
-  createdAt: "2024-01-02T05:24:47.022Z",
-  updatedAt: "2024-01-02T05:24:47.022Z",
+  createdAt: "2024-01-04T00:45:38.030Z",
+  updatedAt: "2024-01-04T00:45:38.030Z",
   __v: 0,
 };
 
