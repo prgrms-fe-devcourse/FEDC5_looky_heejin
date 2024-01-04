0a26ba59e2b00d549a37268d0d172203e8d327b3
Zoody <reopard506@gmail.com>
2024-01-04 14:55:45 +0900

    fix: 파일 및 채널 선택을 필수로 할 수 있도록 버튼 disabled 를 사용

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -51,7 +51,7 @@ const Button: React.FC<ButtonProps> = forwardRef((props, buttonRef) => {
     active,
     width,
     loading = false,
-    disabled = variant === "disabled",
+    disabled,
     useRipple = true,
     rippleColor,
     style = {},
@@ -61,7 +61,9 @@ const Button: React.FC<ButtonProps> = forwardRef((props, buttonRef) => {
     ...rest
   } = props;
   // HTMLButtonElement 에 정의된 속성 이외에 추가적인 속성을 부여하기 위함 <$buttonColor, $textColor>
-  const ButtonWrapper: React.ComponentType<any> = ButtonType[variant];
+  const ButtonWrapper: React.ComponentType<any> = disabled
+    ? ButtonType["disabled"]
+    : ButtonType[variant];
 
   const ref = useRef<typeof ButtonWrapper>(null);
   const rippleRef = useRef<HTMLSpanElement>(null);
