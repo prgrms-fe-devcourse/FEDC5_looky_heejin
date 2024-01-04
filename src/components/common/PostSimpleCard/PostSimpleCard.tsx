e843f0a316b42808c14f7e5c916916afece03a3d
SoJuSo <asdfg7123@gmail.com>
2024-01-04 12:24:46 +0900

    feat: state와 이벤트를 부여하여 기본동작을 수행하도록 수정 design: div태그로 한 번 더 감싸서 배경색상 지정 등 UI 다루는 방식 수정

--- src/components/common/PostSimpleCard/PostSimpleCard.tsx
+++ src/components/common/PostSimpleCard/PostSimpleCard.tsx
@@ -4,10 +4,16 @@ import { _USERDATA } from "@/api/queries/userData";
 import { useMutation } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { Avatar } from "..";
+import useTheme from "@/hooks/useTheme";
+import { useNavigate } from "react-router-dom";
 
 const PostSimpleCard = () => {
+  const navigate = useNavigate();
+  const theme = useTheme();
   const [imageUrl, setimageUrl] = useState<string>();
   const [userName, setUserName] = useState<string>();
+  const [favoriteClicked, setFavoriteClicked] = useState<boolean>(false);
+  const [tagClicked, setTagClicked] = useState<boolean>(false);
   const mutation = useMutation({
     mutationFn: async (params: string) => await _USERDATA(params),
     onSuccess(data) {
@@ -20,6 +26,24 @@ const PostSimpleCard = () => {
       console.error("API 에러: ", error);
     },
   });
+
+  const onClickFavorite = () => {
+    setFavoriteClicked(!favoriteClicked);
+  };
+
+  const onClickTag = () => {
+    setTagClicked(!tagClicked);
+  };
+
+  const onClickComment = () => {
+    // todo
+    alert(`추후 댓글관련 모달 혹은 페이지로 돌려줄 예정입니다!`);
+  };
+
+  const onClickSend = () => {
+    // todo
+    navigate(`/chat/${PostSimpleCardConst.data_example.author}`);
+  };
 
   useEffect(() => {
     mutation.mutate(PostSimpleCardConst.data_example.author);
@@ -45,60 +69,105 @@ const PostSimpleCard = () => {
             />
           </div>
           <CardImageContainer>
-            <Avatar
-              size={"XS"}
-              shape={"round"}
-              src={imageUrl ? imageUrl : ""}
-            ></Avatar>
-            <span
+            <div
+              onClick={onClickSend}
               style={{
+                cursor: "pointer",
                 boxSizing: "border-box",
-                rotate: "-45deg",
                 position: "absolute",
                 top: "5%",
-                // bottom: "0",
                 right: "1%",
               }}
-              className="material-symbols-rounded"
             >
-              send
-            </span>
-            <span
+              <span
+                style={{
+                  rotate: "-45deg",
+                }}
+                className="material-symbols-rounded"
+              >
+                send
+              </span>
+            </div>
+            <div
+              onClick={onClickComment}
               style={{
+                cursor: "pointer",
                 boxSizing: "border-box",
                 position: "absolute",
                 top: "7%",
-                // bottom: "0",
                 right: "14%",
               }}
-              className="material-symbols-rounded"
             >
-              comment
-            </span>
-            <span
+              <span className="material-symbols-rounded">comment</span>
+            </div>
+            <div
+              onClick={onClickTag}
               style={{
+                scale: "1.1",
+                cursor: "pointer",
                 boxSizing: "border-box",
                 position: "absolute",
                 top: "-40%",
                 right: "2%",
+                borderRadius: "50%",
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
               }}
-              className="material-symbols-rounded"
             >
-              sell
-            </span>
-            <span
+              <span
+                style={{
+                  scale: "0.8",
+                  color: !tagClicked ? theme?.gray_100 : theme?.symbol_color,
+                  fontVariationSettings: "fill",
+                }}
+                className="material-symbols-rounded"
+              >
+                sell
+              </span>
+            </div>
+            <div
+              onClick={onClickFavorite}
               style={{
+                scale: "1.1",
+                cursor: "pointer",
                 boxSizing: "border-box",
                 position: "absolute",
                 top: "-80%",
                 right: "2%",
+                borderRadius: "50%",
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
               }}
-              className="material-symbols-rounded"
             >
-              favorite
-            </span>
-            <span>{userName}</span>
-            <span>{PostSimpleCardConst.data_example.title}</span>
+              <span
+                style={{
+                  scale: "0.8",
+                  color: !favoriteClicked
+                    ? theme?.gray_100
+                    : theme?.symbol_color,
+                  fontVariationSettings: "fill",
+                }}
+                className="material-symbols-rounded"
+              >
+                favorite
+              </span>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                margin: "5px",
+              }}
+            >
+              <Avatar
+                size={"S"}
+                shape={"circle"}
+                src={imageUrl ? imageUrl : ""}
+              ></Avatar>
+              <span style={{ marginLeft: "5px" }}>{userName}</span>
+            </div>
+            <div style={{ margin: "5px" }}>
+              <span>{PostSimpleCardConst.data_example.title}</span>
+            </div>
           </CardImageContainer>
         </CardContainer>
       </>
