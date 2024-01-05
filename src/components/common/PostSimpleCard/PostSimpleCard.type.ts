b88fcf58d9e22ae068e9d3ded169c1c5f964948e
SoJuSo <asdfg7123@gmail.com>
2024-01-05 18:22:44 +0900

    feat: 타입 추가 해당 부분은 전역에서 사용될 가능성이 높으므로 추후 전역적으로 돌려주는 부분에 대해 팀원들과 논의 필요

--- src/components/common/PostSimpleCard/PostSimpleCard.type.ts
+++ src/components/common/PostSimpleCard/PostSimpleCard.type.ts
@@ -11,19 +11,94 @@ export interface IPost {
   updatedAt: string;
 }
 
+export interface IChannel {
+  authRequired: boolean;
+  posts: string[];
+  _id: string;
+  name: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IUser {
+  coverImage: string;
+  image: string;
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: IPost[];
+  likes: ILike[];
+  comments: string[];
+  followers: [];
+  following: IFollow[];
+  notifications: Notification[];
+  messages: IMessage[];
+  _id: string;
+  fullName: string;
+  nickname: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface ILike {
   _id: string;
   user: string; // 사용자 id
   post: string; // 포스트 id
   createdAt: string;
   updatedAt: string;
+  __v: number;
 }
 
 export interface IComment {
   _id: string;
   comment: string;
-  author: string;
+  author: IUser; //수정해야함 IUSER인듯?
   post: string; // 포스트 id
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IFollow {
+  _id: string;
+  user: string;
+  follower: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IUser {
+  coverImage: string;
+  image: string;
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: IPost[];
+  likes: ILike[];
+  comments: string[];
+  followers: [];
+  following: IFollow[];
+  notifications: Notification[];
+  messages: IMessage[];
+  _id: string;
+  fullName: string;
+  nickname: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface IMessage {
+  _id: string;
+  message: string;
+  sender: IUser;
+  receiver: IUser;
+  seen: boolean;
   createdAt: string;
   updatedAt: string;
 }
