3ea46e08a7cdbd6d17749e51000a5fb18739c91a
SoJuSo <asdfg7123@gmail.com>
2024-01-05 22:08:50 +0900

    design: 버튼 위치를 퍼센테이지에서 픽셀 절대값으로 변경

--- src/components/common/PostSimpleCard/PostSimpleCard.styles.ts
+++ src/components/common/PostSimpleCard/PostSimpleCard.styles.ts
@@ -3,15 +3,18 @@ import styled, { css } from "styled-components";
 
 export const CardContainer = styled.div<{ $basis: "half" | "full" }>`
   flex-direction: column;
+  box-sizing: border-box;
   ${({ $basis }) => {
     if ($basis === "half") {
       return css`
         max-height: 70%;
+        max-width: calc(100% - 11px);
         flex-basis: calc(50% - 11px);
       `;
     } else {
       return css`
         max-height: 70%;
+        max-width: calc(100% - 11px);
         flex-basis: calc(100% - 11px);
       `;
     }
@@ -52,30 +55,27 @@ export const IconContainer = styled.div<{
     switch ($icon) {
       case "send":
         return css`
-          top: 5%;
-          right: 1%;
+          top: 5px;
+          right: 2px;
         `;
       case "comment":
         return css`
-          top: 7%;
-          right: 14%;
-          @media screen and (max-width: 500px) {
-            right: 10%;
-          }
+          top: 8px;
+          right: 35px;
         `;
       case "sell":
         return css`
           scale: 1.1;
-          top: -40%;
-          right: 2%;
+          bottom: calc(100% + 5px);
+          right: 5px;
           border-radius: 50%;
           background-color: rgba(0, 0, 0, 0.3);
         `;
       case "favorite":
         return css`
           scale: 1.1;
-          top: -80%;
-          right: 2%;
+          bottom: calc(100% + 35px);
+          right: 5px;
           border-radius: 50%;
           background-color: rgba(0, 0, 0, 0.3);
         `;
