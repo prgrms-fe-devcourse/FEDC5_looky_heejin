07142f6710d3e1a084290c25aecc03e279f2131e
SoJuSo <asdfg7123@gmail.com>
2024-01-06 01:03:10 +0900

    feat: 로컬 스토리지 토큰 여부에 따라서 페이지 라우팅 변경

--- src/pages/SplashPage/index.tsx
+++ src/pages/SplashPage/index.tsx
@@ -3,13 +3,14 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { SplashContainer } from "./SplashPage.style";
 import { Image } from "@/components/common";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 const SplashPage = () => {
+  const [token, _] = useLocalStorage("token");
   const navigate = useNavigate();
   useEffect(() => {
     const redirectTimeout = setTimeout(() => {
-      // 2초 후에 /home 경로로 이동
-      navigate("/login");
+      token ? navigate("/home") : navigate("/login");
     }, 2000);
     // 컴포넌트가 언마운트되면 타임아웃을 클리어하여 메모리 누수를 방지합니다.
     return () => clearTimeout(redirectTimeout);
