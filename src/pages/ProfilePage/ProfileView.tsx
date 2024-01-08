51f89ca4249eebcb2f6e4f8eaaff2876670d08c1
suyoung <joyswim.dev@gmail.com>
2024-01-08 18:43:15 +0900

    feat: ProfileView userInfo props 전달

--- src/pages/ProfilePage/ProfileView.tsx
+++ src/pages/ProfilePage/ProfileView.tsx
@@ -1,12 +1,18 @@
 import { useTheme } from "styled-components";
 import { useMe } from "@/hooks/useMe";
-import { useNavigate, useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ICON_SIZE, ICON_SIZE_SMALL } from "./ProfilePage.const";
 import Buttons, { IButtonProps } from "./Buttons";
 import { Avatar, Button } from "@/components/common";
 import { ButtonsWrap, InfoWrap, Profile } from "./ProfilePage.style";
 import Icon from "@/components/common/Icon/Icon";
+import { IUser } from "@/types";
+
+interface IProfileProps {
+  userInfo: IUser;
+  onClickChat: (paramsId: string) => void;
+  [key: string]: any;
+}
 
 const buttonStyle: IButtonProps["style"] = {
   variant: "neumorp",
@@ -18,32 +24,42 @@ const buttonStyle: IButtonProps["style"] = {
   },
 };
 
-const ProfileView = () => {
-  const { id, profilePhoto, userName } = useMe();
-  const theme = useTheme();
-  const { paramsId } = useParams();
-  const [IsMe, _] = useState(id === paramsId);
-  const [IsFollow, setIsFollow] = useState(true);
+const ProfileView = ({
+  userInfo,
+  onClickChat,
+  ...props
+}: IProfileProps) => {
+  const { _id, email, fullName } = userInfo;
+  const { id, profilePhoto } = useMe();
+  const [IsMe, setIsMe] = useState(id === _id);
+  const [IsFollow, setIsFollow] = useState(false);
 
-  const navigate = useNavigate();
+  const theme = useTheme();
+
+  useEffect(() => {
+    setIsMe(id === _id);
+  }, [_id]);
 
   return (
     <Profile>
       {IsMe && (
         <ButtonsWrap className="me">
           <Buttons style={buttonStyle}>
-            <Icon name="Password" size={ICON_SIZE_SMALL} />
-            <Icon name="Password" size={ICON_SIZE_SMALL} />
-            <Icon name="Password" size={ICON_SIZE_SMALL} />
+            <Icon
+              name="Password"
+              size={ICON_SIZE_SMALL}
+            />
           </Buttons>
         </ButtonsWrap>
       )}
-      <InfoWrap>
+      <InfoWrap {...props}>
         <Avatar src={profilePhoto ?? undefined} size="XL"></Avatar>
-        <div style={{ color: theme?.white_primary, paddingTop: "1rem" }}>
-          test@gmail.com
+        <div style={{ color: theme.white_primary, paddingTop: "1rem" }}>
+          {email}
         </div>
-        <span style={{ padding: "0.5rem 0" }}>{userName}</span>
+        <span style={{ color: theme.white_primary, padding: "0.5rem 0" }}>
+          {fullName}
+        </span>
         {/* 닉네임 수정 버튼 */}
         {IsMe && (
           <ButtonsWrap className="me">
@@ -62,10 +78,7 @@ const ProfileView = () => {
         {!IsMe && (
           <ButtonsWrap className="others">
             <Buttons style={buttonStyle}>
-              <Icon
-                name="chat_bubble"
-                onClick={() => navigate(`/chat/${paramsId}`)}
-              />
+              <Icon name="chat_bubble" onClick={() => onClickChat(_id)} />
               <Icon
                 name={!IsFollow ? "person_add" : "person_check"}
                 // 확인하기
