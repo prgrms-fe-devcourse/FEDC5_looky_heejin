b79feb204707172a8057b7dfbf88483c3f53980a
SoJuSo <asdfg7123@gmail.com>
2024-01-08 21:23:38 +0900

    feat: 댓글 관련 api 기능 추가

--- src/api/queries/comment.ts
+++ src/api/queries/comment.ts
@@ -1,7 +1,12 @@
-import { IComment } from "@/types";
+import type { ICreateComment, IDeleteComment } from "@/types";
 import { _POST } from "..";
 
-export const _COMMENT = async (params: IComment) => {
+export const _CREATE_COMMENT = async (params: ICreateComment) => {
   const result = await _POST("/comments/create", params);
+  return result?.data;
+};
+
+export const _DELETE_COMMENT = async (params: IDeleteComment) => {
+  const result = await _POST("/comments/delete", params);
   return result?.data;
 };
