155b378bbc0691b2a9e98a214c84d8db6e4bed28
suyoung <joyswim.dev@gmail.com>
2024-01-08 18:00:57 +0900

    feat:  _GET_USER queryKey 추가

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,5 +1,6 @@
 export const GET_CHANNELS = "channels";
+export const GET_USER = "user";
 export const ME = "me";
-export const NOTIFICATION = "notification"
+export const NOTIFICATION = "notification";
 export const CHATS = "chats";
 export const CHAT = "chat";
