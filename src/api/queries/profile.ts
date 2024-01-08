da1045f45f330f27ff4c6b34e41a4432d0af8933
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-09 01:12:19 +0900

    feat: 프로필 API 추가

--- src/api/queries/profile.ts
+++ src/api/queries/profile.ts
@@ -1,9 +1,22 @@
-import { _GET } from "@/api";
+import { _DELETE, _GET, _POST } from "@/api";
+import { IFollow, IFollowResponse, IUnFollow } from "@/types/profile";
 
 export const _GET_USER = async (userId: string) => {
   if (!userId) {
     throw new Error("Query is Required");
   }
   const results = await _GET(`/users/${userId}`);
+  return results?.data;
+};
+
+export const _FOLLOW = async (params: IFollow): Promise<IFollowResponse> => {
+  const results = await _POST("/follow/create", params);
+  return results?.data;
+};
+
+export const _UNFOLLOW = async (
+  params: IUnFollow
+): Promise<IFollowResponse> => {
+  const results = await _DELETE("/follow/delete", params);
   return results?.data;
 };
