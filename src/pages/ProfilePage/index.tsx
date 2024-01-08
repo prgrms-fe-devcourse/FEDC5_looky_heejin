45a2e91210084f54d601d41ef680a55f2bed0420
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-09 01:29:23 +0900

    feat: 팔로우 버튼 클릭

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -2,23 +2,49 @@ import styled from "styled-components";
 import ProfileView from "./ProfileView";
 import ProfilePostsView from "./ProfilePostsView";
 import { useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { GET_USER } from "@/constants/queryKey";
-import { _SEARCH_USERS } from "@/api/queries/search";
-import { _GET_USER } from "@/api/queries/profile";
+import { _FOLLOW, _GET_USER, _UNFOLLOW } from "@/api/queries/profile";
 import { useEffect, useState } from "react";
 import { IUser } from "@/types";
+import { IFollow, IUnFollow } from "@/types/profile";
 
 const ProfileWrap = styled.div`
   overflow-y: scroll;
 `;
 
 const ProfilePage = () => {
+  const { id } = useParams();
   const [userData, setUserData] = useState<IUser>();
   const [isFollow, setIsFollow] = useState(false);
-  const { id } = useParams();
+  const [formData, setFormData] = useState<IFollow | IUnFollow>();
 
   const navigate = useNavigate();
+
+  const followMutation = useMutation({
+    mutationFn: async (formData: IFollow) => {
+      return await _FOLLOW(formData);
+    },
+    onSuccess(data) {
+      console.log(data);
+    },
+    onError(error) {
+      console.error("ERROR: ", error);
+    },
+  });
+
+  const unfollowMutation = useMutation({
+    mutationFn: async (formData: IUnFollow) => {
+      return await _UNFOLLOW(formData);
+    },
+    onSuccess(data) {
+      console.log("API 성공! UnFollow");
+      console.log(data);
+    },
+    onError(error) {
+      console.error("ERROR: ", error);
+    },
+  });
 
   const { data, isLoading, error, isSuccess } = useQuery({
     queryKey: [GET_USER, id],
@@ -50,9 +76,20 @@ const ProfilePage = () => {
   };
 
   const handleClickFollow = (e: React.MouseEvent) => {
-    console.log("팔로우 클릭!");
     e.stopPropagation();
     setIsFollow(!isFollow);
+
+    if (id === undefined) return;
+
+    setFormData({
+      userId: id,
+    });
+
+    if (!isFollow) {
+      followMutation.mutate(formData as IFollow);
+    } else {
+      unfollowMutation.mutate(formData as IUnFollow);
+    }
   };
 
   useEffect(() => {
