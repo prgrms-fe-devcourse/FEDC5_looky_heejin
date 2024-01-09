e18a98f0cd4f473ac3c7f1a0bcc2c34503590d7f
SoJuSo <asdfg7123@gmail.com>
2024-01-09 12:45:07 +0900

    feat: 채널을 선택 후 로컬스토리지에 채널 값을 저장하는 로직 구현

---
+++ src/pages/ChannelsPage/ChannelsPage.view.tsx
@@ -0,0 +1,36 @@
+import { _GET } from "@/api";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { ChannelStyle } from "./ChannelsPage.styles";
+
+const ChannelsPage = () => {
+  const [_, setChannel] = useLocalStorage("ViewChannel");
+  const navigate = useNavigate();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["channels"],
+    queryFn: async () => await _GET("/channels"),
+  });
+
+  const handleClick = (target: string) => {
+    navigate("/home");
+    setChannel(target);
+  };
+
+  if (data?.data) {
+    return (
+      <>
+        {data?.data.map((val: any) => {
+          return (
+            <ChannelStyle key={val._id} onClick={() => handleClick(val.name)}>
+              {val.name}
+            </ChannelStyle>
+          );
+        })}
+      </>
+    );
+  }
+};
+
+export default ChannelsPage;
