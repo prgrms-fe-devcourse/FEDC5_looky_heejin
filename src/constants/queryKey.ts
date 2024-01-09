6577b1733d9bf1cd67edfcacc926c3ed028c87f4
Zoody <reopard506@gmail.com>
2024-01-09 11:08:10 +0900

    fit: 실제 데이터 polling
    - 30 초마다 풀링하는 것으로 초기 설정하였습니다.

--- src/constants/queryKey.ts
+++ src/constants/queryKey.ts
@@ -1,6 +1,4 @@
 export const GET_CHANNELS = "channels";
-
 export const ME = "me";
-
 export const CHATS = "chats";
 export const CHAT = "chat";
--- src/pages/NotificationsPage/NotificationsPage.view.tsx
+++ src/pages/NotificationsPage/NotificationsPage.view.tsx
@@ -1,13 +1,23 @@
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 import { useNotification } from "@/hooks/useNotification";
 import NotificationCard from "@/components/Notification/NotificationCard/NotificationCard";
 import { INotification } from "@/types/notification";
+import { useMutation } from "@tanstack/react-query";
+import { _PUT } from "@/api";
 
 const NotificationsPage = () => {
   const { common } = useNotification();
 
-  // console.log(common);
+  const mutation = useMutation({
+    mutationFn: async () => await _PUT("/notifications/seen"),
+  });
+
+  useEffect(() => {
+    mutation.mutate();
+
+    return () => mutation.mutate();
+  }, []);
 
   const getType = useCallback(
     (data: INotification) => {
@@ -21,7 +31,7 @@ const NotificationsPage = () => {
 
   return (
     <ul className="space-y-6 pt-8">
-      {common.map(data => (
+      {common?.map(data => (
         <NotificationCard key={data._id} type={getType(data)} data={data} />
       ))}
     </ul>
