ddb69d20ebb404999cf99bd558f778b2fe667955
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-09 13:13:28 +0900

    feat: 닉네임 변경 validation 상수 추가

--- src/pages/ProfilePage/ProfilePage.const.ts
+++ src/pages/ProfilePage/ProfilePage.const.ts
@@ -1,2 +1,17 @@
 export const ICON_SIZE_SMALL = 25;
 export const ICON_SIZE = 40;
+
+export const NAME_VALIDATION = {
+  required: {
+    message: "변경할 닉네임을 입력하세요",
+    value: false,
+  },
+  minLength: {
+    message: "닉네임은 2글자 이상입니다",
+    value: 2,
+  },
+  maxLength: {
+    message: "닉네임은 20글자 이하입니다",
+    value: 20,
+  },
+};
