daa82fad535c390b197ab7549e8f5cecc55010d5
SoJuSo <asdfg7123@gmail.com>
2024-01-09 11:32:56 +0900

    fix: 화면 정의 변경에 따른 일부 기능 수정, 제거

--- src/components/common/PostSimpleCard/PostSimpleCard.tsx
+++ src/components/common/PostSimpleCard/PostSimpleCard.tsx
@@ -9,12 +9,14 @@ import {
 } from "./PostSimpleCard.styles";
 import PostSimpleCardConst from "./PostSimpleCard.const";
 import { _USERDATA } from "@/api/queries/userData";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { Avatar } from "..";
 import useTheme from "@/hooks/useTheme";
 import { useNavigate } from "react-router-dom";
 import type { ITag } from "@/types/post";
+import { _GET } from "@/api";
+import { ME } from "@/constants/queryKey";
 export interface ITitle {
   title: string;
   content: string;
@@ -31,12 +33,20 @@ const IsJsonString = (str: string) => {
 };
 
 // todo, 타입 충돌로 인해서 추후 타입 명시
-const PostSimpleCard = ({ postData }: { postData: any }) => {
+const PostSimpleCard = ({ postData }: { key: number; postData: any }) => {
+  console.log(postData);
+  const { data: myData } = useQuery({
+    queryKey: [ME],
+    queryFn: async () => await _GET("/auth-user"),
+  });
+  console.log(myData);
+
   let parsedData: ITitle = {
     title: "",
     content: "",
     tags: null,
   };
+
   if (IsJsonString(postData.title)) {
     const parsedJson = JSON.parse(postData.title);
     parsedData = parsedJson;
@@ -46,7 +56,6 @@ const PostSimpleCard = ({ postData }: { postData: any }) => {
   const [imageUrl, setimageUrl] = useState<string>();
   const [userName, setUserName] = useState<string>();
   const [favoriteClicked, setFavoriteClicked] = useState<boolean>(false);
-  const [tagClicked, setTagClicked] = useState<boolean>(false);
   const mutation = useMutation({
     mutationFn: async (params: string) => await _USERDATA(params),
     onSuccess(data) {
@@ -58,26 +67,17 @@ const PostSimpleCard = ({ postData }: { postData: any }) => {
     },
   });
 
+  const onClickImage = () => {
+    // todo
+    // 오픈 모달로 변경 예정
+  };
+
   const onClickProfile = () => {
-    navigate(`/profile/${PostSimpleCardConst.data_example.author}`);
+    navigate(`/profile/${postData.author._id}`);
   };
 
   const onClickFavorite = () => {
     setFavoriteClicked(!favoriteClicked);
-  };
-
-  const onClickTag = () => {
-    setTagClicked(!tagClicked);
-  };
-
-  const onClickComment = () => {
-    // todo
-    alert(`추후 댓글관련 모달 혹은 페이지로 돌려줄 예정입니다!`);
-  };
-
-  const onClickSend = () => {
-    // todo
-    navigate(`/chat/${postData.author._id}`);
   };
 
   useEffect(() => {
@@ -98,39 +98,19 @@ const PostSimpleCard = ({ postData }: { postData: any }) => {
     return (
       <>
         <CardContainer $basis="half">
-          <CardImageContainer style={{ minHeight: "200px", minWidth: "200px" }}>
+          <CardImageContainer style={{ minHeight: "200px", minWidth: "100%" }}>
             {/* todo, 카드 컴포넌트 원주님과 협업 후 공용 컴포넌트로 변경 */}
             <CardImage
-              src={postData.image ? postData.image : ""}
+              onClick={onClickImage}
+              src={
+                postData.image
+                  ? postData.image
+                  : "https://cdn.pixabay.com/photo/2022/01/17/22/20/subtract-6945896_1280.png"
+              }
               alt="포스팅 이미지"
             />
           </CardImageContainer>
           <CardInfoContainer>
-            <IconContainer $icon="send" onClick={onClickSend}>
-              <span
-                style={{
-                  rotate: "-45deg",
-                }}
-                className="material-symbols-rounded"
-              >
-                send
-              </span>
-            </IconContainer>
-            <IconContainer $icon="comment" onClick={onClickComment}>
-              <span className="material-symbols-rounded">comment</span>
-            </IconContainer>
-            <IconContainer $icon="sell" onClick={onClickTag}>
-              <span
-                style={{
-                  scale: "0.8",
-                  color: !tagClicked ? theme?.gray_100 : theme?.symbol_color,
-                  fontVariationSettings: "fill",
-                }}
-                className="material-symbols-rounded"
-              >
-                sell
-              </span>
-            </IconContainer>
             <IconContainer $icon="favorite" onClick={onClickFavorite}>
               <span
                 style={{
@@ -175,9 +155,11 @@ const PostSimpleCard = ({ postData }: { postData: any }) => {
                 flexDirection: "column",
                 boxSizing: "border-box",
                 outline: "none",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              <TextContainer>
+              <TextContainer style={{ fontSize: "1.2rem" }} $why={true}>
                 {parsedData === null ? postData.title : parsedData.title}
               </TextContainer>
               <TextContainer>
