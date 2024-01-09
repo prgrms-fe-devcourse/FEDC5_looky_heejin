89c4d2f903d96312aafef12b4e551a7ca4ab9588
Suyoung Shin <joyswim.dev@gmail.com>
2024-01-09 13:01:04 +0900

    feat: EditNameModal 모달 추가

---
+++ src/components/modalViews/EditNameModal.tsx
@@ -0,0 +1,68 @@
+import { FieldErrors, useForm } from "react-hook-form";
+import { Button, Input, InputLabel } from "../common";
+import styled from "styled-components";
+import { Col } from "@/styles/GlobalStyle";
+import { useUI } from "../common/uiContext";
+import { NAME_VALIDATION } from "@/pages/ProfilePage/ProfilePage.const";
+import { useMutation } from "@tanstack/react-query";
+import { _UPDATE_NAME } from "@/api/queries/profile";
+
+interface INameFormProps {
+  fullName: string;
+}
+
+const Form = styled(Col)`
+  justify-content: center;
+  width: 300px;
+  height: 300px;
+`;
+
+const InputWrap = styled.div`
+  padding-bottom: 1rem;
+`;
+
+const EditNameModal = () => {
+  const { closeModal } = useUI();
+  const { register, handleSubmit } = useForm<INameFormProps>();
+
+  const mutation = useMutation({
+    mutationFn: async (updatedName: string) =>
+      await _UPDATE_NAME({ fullName: updatedName }),
+    onSuccess: () => {
+      console.log("API UPDATE NAME 성공!");
+    },
+    onError: error => console.log("Error", error),
+  });
+
+  const onValid = (data: INameFormProps) => {
+    mutation.mutate(data.fullName);
+    closeModal();
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.warn(errors);
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit(onValid, onInvalid)}>
+        <InputWrap>
+          <InputLabel title="변경할 닉네임" help="2글자 이상" />
+          <Input
+            type="text"
+            register={register("fullName", NAME_VALIDATION)}
+            required={false}
+          />
+        </InputWrap>
+        <Button
+          variant="symbol"
+          type="submit"
+          onClick={handleSubmit(onValid, onInvalid)}
+        >
+          변경
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default EditNameModal;
--- src/components/modalViews/index.ts
+++ src/components/modalViews/index.ts
@@ -1,3 +1,4 @@
 export { default as ChannelSelectModal } from "./ChannelSelectModal";
 export { default as TestModal } from "./TestModal";
 export { default as TagCreateModal } from "./TagCreateModal";
+export { default as EditNameModal } from "./EditNameModal";
