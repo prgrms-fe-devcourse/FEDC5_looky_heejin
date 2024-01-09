9c14308eaf148701f88d72568bb6e3dd0c0271dd
SoJuSo <asdfg7123@gmail.com>
2024-01-09 13:05:09 +0900

    feat: 홈페이지에서 저장된 페이지의 목록에 따라 심플 카드를 보여주도록 출력

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,50 @@
+import PostSimpleCard from "@/components/common/PostSimpleCard";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { _GET } from "@/api";
+import { useMutation } from "@tanstack/react-query";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+
+const Container = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0px -5.5px;
+  width: calc(100% + 11px);
+  padding-bottom: 3rem;
+`;
+
 const Home = () => {
-  return (
-    <>
-      <div style={{ height: "10000px" }}>홈페이지</div>
-    </>
-  );
+  const [channel, _] = useLocalStorage("ViewChannel");
+  const [data, setData] = useState<any[]>([]);
+
+  const initMutation = useMutation({
+    mutationFn: async (endPoint: string) => await _GET(endPoint),
+    onSuccess(data) {
+      if (data) setData(data.data);
+    },
+    onError(error) {
+      console.log("API 에러: ", error);
+    },
+  });
+
+  useEffect(() => {
+    // 궁금증
+    // channel을 받아오고 실행해야 하는것이 아닌지?
+    if (channel) initMutation.mutate(`/posts/channel/${channel}`);
+  }, []);
+
+  if (data)
+    return (
+      <>
+        <Container>
+          {data.map((value, index) => (
+            <PostSimpleCard key={index} postData={value} />
+          ))}
+        </Container>
+      </>
+    );
 };
 
 export default Home;
