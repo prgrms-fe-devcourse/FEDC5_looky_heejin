f283d0c69240ad0a970f23caedf389e26b4009cc
hellosonic-r <hellommmh@gmail.com>
2024-01-10 14:27:22 +0900

    hotfix : error 구문 제거

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Layout from "@/components/common/Layout";
 import {
