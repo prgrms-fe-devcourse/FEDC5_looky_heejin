370a44ad12053b531f1766eb0e2e4e19df899843
Zoody <reopard506@gmail.com>
2024-01-11 09:30:02 +0900

    fix: 알림 - 포스트 디테일 모달 뷰 연결

--- src/pages/NotificationsPage/NotificationsPage.view.tsx
+++ src/pages/NotificationsPage/NotificationsPage.view.tsx
@@ -5,8 +5,12 @@ import NotificationCard from "@/components/Notification/NotificationCard/Notific
 import { INotification } from "@/types/notification";
 import { useMutation } from "@tanstack/react-query";
 import { _PUT } from "@/api";
+import { useNavigate } from "react-router-dom";
+import { useUI } from "@/components/common/uiContext";
 
 const NotificationsPage = () => {
+  const navigate = useNavigate();
+  const { setModalView, openModal } = useUI();
   const { common, initCommonCount } = useNotification();
 
   const mutation = useMutation({
@@ -26,17 +30,35 @@ const NotificationsPage = () => {
   const getType = useCallback(
     (data: INotification) => {
       if (data.comment) return "comment";
-      else if (data.follow) return "comment";
+      else if (data.follow) return "follow";
       else if (data.like) return "like";
       return "post";
     },
     [common]
   );
 
+  const cardClickHandler = useCallback(
+    (data: INotification, isProfile?: boolean) => {
+      const target = getType(data);
+      if (isProfile || target === "follow") {
+        navigate(`/profile/${data.author._id}`);
+      } else {
+        setModalView("POST_DETAIL_VIEW");
+        openModal({ postId: data.post });
+      }
+    },
+    []
+  );
+
   return (
     <ul className="space-y-6 pt-8">
       {common?.map(data => (
-        <NotificationCard key={data._id} type={getType(data)} data={data} />
+        <NotificationCard
+          key={data._id}
+          type={getType(data)}
+          data={data}
+          onClickHandler={cardClickHandler}
+        />
       ))}
     </ul>
   );
