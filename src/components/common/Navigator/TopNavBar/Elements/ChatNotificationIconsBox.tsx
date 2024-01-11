31539f2f8e17c8aa0765b057a16369ac3174ba30
SoJuSo <asdfg7123@gmail.com>
2024-01-11 16:44:18 +0900

    feat: 테마 변경 버튼 추가

--- src/components/common/Navigator/TopNavBar/Elements/ChatNotificationIconsBox.tsx
+++ src/components/common/Navigator/TopNavBar/Elements/ChatNotificationIconsBox.tsx
@@ -1,7 +1,13 @@
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import Icon from "../../../Icon/Icon";
-import { CHAT_ICON, NOTIFICATIONS_ICON } from "@/constants/icons";
+import {
+  CHAT_ICON,
+  DARKMODE_ICON,
+  LIGHTMODE_ICON,
+  NOTIFICATIONS_ICON,
+} from "@/constants/icons";
 import { PathName } from "@/constants/pathNameConstants";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
 
 interface IChatNotificationIconsBoxProps {
   onClick: (path: string) => void;
@@ -11,8 +17,9 @@ const IconsBox = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  gap: 5px;
   margin: auto 0;
-  & > :first-child {
+  & > :nth-child(2) {
     padding-top: 0.3rem;
   }
 `;
@@ -27,8 +34,22 @@ const IconWrapper = styled.div`
 const ChatNotificationIconsBox = ({
   onClick,
 }: IChatNotificationIconsBoxProps) => {
+  const theme = useTheme();
+  const [_, setLocalTheme] = useLocalStorage("theme");
+
+  const handleTheme = () => {
+    setLocalTheme(theme.theme_mode === "light" ? "dark" : "light");
+  };
+
   return (
     <IconsBox>
+      <IconWrapper onClick={handleTheme}>
+        <Icon
+          name={theme.theme_mode === "light" ? LIGHTMODE_ICON : DARKMODE_ICON}
+          size="1.6rem"
+          weight={250}
+        ></Icon>
+      </IconWrapper>
       <IconWrapper onClick={() => onClick(PathName.CHATS)}>
         <Icon name={CHAT_ICON} size="1.6rem" weight={250}></Icon>
       </IconWrapper>
