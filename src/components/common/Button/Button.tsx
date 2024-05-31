1898794d81e663b72b4dab7944e1a980c7ab19df
Suyoung Shin <joyswim.dev@gmail.com>
2024-05-31 13:47:11 +0900

    [🔥remove]: 안 쓰는 변수 삭제

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -11,7 +11,6 @@ import cn from "clsx";
 
 import { ButtonRipple, Disabled, Flat, Neumorp, Symbol } from "./Button.styles";
 import rippleStyle from "./Button.module.css";
-import { NAV_TITLE } from "@/constants";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   href?: string;
