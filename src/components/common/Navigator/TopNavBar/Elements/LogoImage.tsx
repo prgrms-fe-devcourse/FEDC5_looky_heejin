4ba403a7cb9f3e91a1d9cdfe6a2561fc08e75cb2
suyoung <joyswim.dev@gmail.com>
2024-07-16 23:44:25 +0900

    [♻️refactor]: 로고 이미지 Image 컴포넌트 변경

--- src/components/common/Navigator/TopNavBar/Elements/LogoImage.tsx
+++ src/components/common/Navigator/TopNavBar/Elements/LogoImage.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import Logo from "/logo.png";
+import { Image } from "@/components/common";
 
 interface ILogoImageProps {
   onClick: React.MouseEventHandler<HTMLImageElement>;
 }
 
-const StyledImg = styled.img`
+const StyledImg = styled.div`
+  position: relative;
   width: 4rem;
   margin: auto auto;
   cursor: pointer;
+  aspect-ratio: 4 / 1.78;
 `;
 
 const LogoImage = ({ onClick }: ILogoImageProps) => {
-  return <StyledImg src={Logo} alt="루키 로고" onClick={onClick} />;
+  return (
+    <StyledImg onClick={onClick}>
+      <Image
+        src={Logo}
+        alt="루키 로고"
+        fill={true}
+        style={{ objectFit: "cover" }}
+      />
+    </StyledImg>
+  );
 };
 
 export default LogoImage;
