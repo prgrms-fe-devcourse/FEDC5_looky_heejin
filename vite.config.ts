71a91b8e9acadf85abfa606aff6ac5e71829a7b8
suyoung <joyswim.dev@gmail.com>
2024-07-17 02:20:41 +0900

    [🔨chore]: vite.config.ts에 viteCompression플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,9 +20,12 @@ export default defineConfig({
       "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
-  plugins: [react({
-    babel: {
-      plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
-    },
-  })],
+  plugins: [
+    react({
+      babel: {
+        plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
+      },
+    }),
+    viteCompression(),
+  ],
 });
