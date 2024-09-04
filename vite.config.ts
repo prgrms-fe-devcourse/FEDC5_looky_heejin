ee06e96dc328aeff3d55e37fa4b0e78c239d9055
suyoung <joyswim.dev@gmail.com>
2024-09-04 21:43:52 +0900

    [♻️refactor]: gzip, br 압축 알고리즘 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -26,6 +26,7 @@ export default defineConfig({
         plugins: ["babel-plugin-macros", "babel-plugin-styled-components"],
       },
     }),
-    viteCompression(),
+    compression(),
+    compression({ algorithm: "brotliCompress" }),
   ],
 });
